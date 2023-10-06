aad555e3c53e179d98a0cea874f2624c8e063945
Pratigya Panwar <102457440+panwar8279@users.noreply.github.com>
2023-10-06 12:39:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ window.addEventListener("mousemove", function(details){
  
     let rect=document.querySelector("#rect");
 
-    var xval=gsap.utils.mapRange(0, window.innerWidth,100 + rect.getBoundingClientRect().width/2 ,window.innerWidth- (100+rect.getBoundingClientRect().width/2),details.clientX)
+    let xval=gsap.utils.mapRange(0, window.innerWidth,100 + rect.getBoundingClientRect().width/2 ,window.innerWidth- (100+rect.getBoundingClientRect().width/2),details.clientX)
 
+    let yval=gsap.utils.mapRange(0, window.innerWidth,100 + rect.getBoundingClientRect().width/2 ,window.innerWidth- (100+rect.getBoundingClientRect().width/2),details.clientY)
     gsap.to("#rect", {
         left: xval +"px",
+        top: yval +"px",
         ease: Power3
     })
 })
